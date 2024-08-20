3c918d6e09b56c70cdecad32a590435eb715c587
kjvmartinez <kjmartinez@psu.palawan.edu.ph>
2024-08-20 10:24:28 +0800

    Add other console app

---
+++ PS01_5_SwitchDays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS01_5_SwitchDays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
