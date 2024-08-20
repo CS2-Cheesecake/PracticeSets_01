af9f40925776ab7bb3c741e3e150ea97c9d52796
kjvmartinez <kjmartinez@psu.palawan.edu.ph>
2024-08-20 10:14:18 +0800

    first commit

---
+++ PS01_2_OddEven/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PS01_2_OddEven
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
